ca8ecf9b9f29c706549b8fa7197ceb5746981e14
Maksim Shuklin <makshuk@my.com>
2023-09-05 08:26:19 +0300

    type safety update

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-	imports: [OpenaiModule],
+	imports: [OpenaiModule, ConfigModule.forRoot()],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -4,15 +4,16 @@ import OpenAI from 'openai';
 @Injectable()
 export class OpenaiService {
 	openai = new OpenAI({
-		apiKey: 'my api key', // defaults to process.env["OPENAI_API_KEY"]
+		apiKey: process.env.OPENAI_API_KEY,
 	});
 
 	async main() {
 		const completion = await this.openai.chat.completions.create({
-			messages: [{ role: 'user', content: 'Say this is a test' }],
+			messages: [{ role: 'user', content: 'hi' }],
 			model: 'gpt-3.5-turbo',
 		});
 
 		console.log(completion.choices);
+		return completion;
 	}
 }
