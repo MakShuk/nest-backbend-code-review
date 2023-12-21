5ff3ed65b04e4d8321e8ee6a6369e9785a0574b4
Maksim Shuklin <makshuk@my.com>
2023-12-21 21:55:03 +0300

    save

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,12 +1,48 @@
-import { Controller, Get } from '@nestjs/common';
-import { OpenaiService } from './openai.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { OpenaiService, ExtendedChatCompletionMessage } from './openai.service';
+import fs, { ReadStream } from 'fs';
+import { Stream } from 'openai/streaming';
+import { Response } from 'express';
+import { ChatCompletionChunk } from 'openai/resources/chat';
 
 @Controller('openai')
 export class OpenaiController {
 	constructor(private readonly openaiService: OpenaiService) {}
 
-	@Get()
-	findAll() {
-		return this.openaiService.main();
+	@Get('response')
+	async response(): Promise<ExtendedChatCompletionMessage> {
+		const messages = [];
+		messages.push(this.openaiService.createUserMessage('1 афоризм'));
+		const responce = await this.openaiService.response(messages);
+		if (responce.error) console.log('Ошибка');
+		return responce;
+	}
+
+	@Get('stream-respose')
+	async streamResponse(@Res() res: Response): Promise<Stream<ChatCompletionChunk> | string> {
+		const message = this.openaiService.createUserMessage('Один афоризм');
+		const yourStream = await this.openaiService.streamResponse([message]);
+		console.log(yourStream);
+		if (yourStream instanceof Stream) {
+			res.setHeader('Content-Type', 'application/octet-stream');
+			res.setHeader('Content-Disposition', 'attachment; filename=stream.txt');
+			/* for await (const part of yourStream) {
+				process.stdout.write(part.choices[0]?.delta?.content || '');
+			} */
+			return yourStream;
+		} else {
+			return yourStream.content || 'Нет данных';
+		}
+	}
+
+	@Get('file-uploads')
+	fileUploads(): Promise<void> {
+		return this.openaiService.fileUploads();
+	}
+
+	@Get('transcription-audio')
+	transcriptionAudio(): Promise<ExtendedChatCompletionMessage> {
+		const stream: ReadStream = fs.createReadStream('audio.mp3');
+		return this.openaiService.transcriptionAudio(stream);
 	}
 }
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,5 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import OpenAI from 'openai';
+import {
+	ChatCompletionChunk,
+	ChatCompletionMessage,
+	ChatCompletionMessageParam,
+} from 'openai/resources/chat';
+import { ReadStream } from 'fs';
+import { Stream } from 'openai/streaming';
+
+export interface ExtendedChatCompletionMessage extends ChatCompletionMessage {
+	error?: boolean;
+}
 
 @Injectable()
 export class OpenaiService {
@@ -7,13 +18,109 @@ export class OpenaiService {
 		apiKey: process.env.OPENAI_API_KEY,
 	});
 
-	async main() {
-		const completion = await this.openai.chat.completions.create({
-			messages: [{ role: 'user', content: 'hi' }],
-			model: 'gpt-3.5-turbo',
-		});
+	async response(messages: ChatCompletionMessageParam[]): Promise<ExtendedChatCompletionMessage> {
+		try {
+			const completion = await this.openai.chat.completions.create({
+				messages: messages,
+				model: 'gpt-3.5-turbo',
+			});
+			if (!completion.choices[0]?.message) throw new Error('openai.chat.completions is undefined');
+			return completion.choices[0]?.message;
+		} catch (error) {
+			if (error instanceof OpenAI.APIError) {
+				const { status, message, code, type } = error;
+				const errorMessage = `status: ${status} message: ${message} code: ${code} type: ${type}`;
+				console.error(errorMessage);
+				return {
+					role: 'assistant',
+					content: errorMessage,
+					error: true,
+				};
+			} else {
+				return {
+					role: 'assistant',
+					content: `Non-API error, ${error}`,
+					error: true,
+				};
+			}
+		}
+	}
 
-		console.log(completion.choices);
-		return completion;
+	async streamResponse(
+		messages: ChatCompletionMessageParam[],
+	): Promise<Stream<ChatCompletionChunk> | ExtendedChatCompletionMessage> {
+		try {
+			const stream = await this.openai.chat.completions.create({
+				model: 'gpt-3.5-turbo',
+				messages: messages,
+				stream: true,
+			});
+			return stream;
+		} catch (error) {
+			if (error instanceof OpenAI.APIError) {
+				const { status, message, code, type } = error;
+				const errorMessage = `status: ${status} message: ${message} code: ${code} type: ${type}`;
+				console.error(errorMessage);
+				return {
+					role: 'assistant',
+					content: errorMessage,
+					error: true,
+				};
+			} else {
+				return {
+					role: 'assistant',
+					content: `Non-API error, ${error}`,
+					error: true,
+				};
+			}
+		}
+	}
+
+	async transcriptionAudio(audioStream: ReadStream): Promise<ExtendedChatCompletionMessage> {
+		try {
+			const response = await this.openai.audio.transcriptions.create({
+				model: 'whisper-1',
+				file: audioStream,
+			});
+			return {
+				role: 'assistant',
+				content: response.text,
+			};
+		} catch (error) {
+			if (error instanceof OpenAI.APIError) {
+				const { status, message, code, type } = error;
+				const errorMessage = `status: ${status} message: ${message} code: ${code} type: ${type}`;
+				console.error(errorMessage);
+				return {
+					role: 'assistant',
+					content: errorMessage,
+					error: true,
+				};
+			} else {
+				return {
+					role: 'assistant',
+					content: `Non-API error, ${error}`,
+					error: true,
+				};
+			}
+		}
+	}
+
+	async fileUploads(): Promise<void> {
+		console.log('lol');
+	}
+
+	createUserMessage(message: string): ChatCompletionMessageParam {
+		return {
+			role: 'user',
+			content: message,
+		};
+	}
+
+	createAssistantMessage(message: string): ChatCompletionMessageParam {
+		return {
+			role: 'assistant',
+			content: message,
+		};
 	}
 }
