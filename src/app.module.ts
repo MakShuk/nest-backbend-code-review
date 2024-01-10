14566de67ea4bc57fe652aecea9f74ae8d38bcdd
Maksim Shuklun <makshuk@bk.ru>
2024-01-10 20:48:15 +0300

    add file service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
 import { ConfigModule } from '@nestjs/config';
+import { PagesModule } from './pages/pages.module';
 
 @Module({
-	imports: [OpenaiModule, ConfigModule.forRoot()],
+	imports: [OpenaiModule, ConfigModule.forRoot(), PagesModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-	const port = 3000;
+	const port = 3333;
 	const app = await NestFactory.create(AppModule);
 	await app.listen(port);
 	console.log(`update-service запущен на ${port} порту`);
