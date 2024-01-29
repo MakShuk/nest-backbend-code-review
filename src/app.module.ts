7d0857d76ca4e819db5a4964d71a76fe799cd5b3
Maksim Shuklin <makshuk@my.com>
2024-01-29 20:56:32 +0300

    deleted middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,13 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OpenaiModule } from './openai/openai.module';
 import { ConfigModule } from '@nestjs/config';
 import { PagesModule } from './file/file.module';
-import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
 	imports: [OpenaiModule, ConfigModule.forRoot(), PagesModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
-export class AppModule {
-	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-	configure(consumer: MiddlewareConsumer) {
-		consumer
-			.apply(LoggerMiddleware) // Подключаем промежуточное программное обеспечение AuthMiddleware
-			.forRoutes('*'); // Для всех маршрутов, начинающихся с '/private'
-	}
-}
+export class AppModule {}
